acc4195f32f996f4aff89b23eefd750db6e044ba
zoon1y <3124317245@qq.com>
2025-11-05 10:49:29 +0800

    Key Changed

--- Hardware/Key.c
+++ Hardware/Key.c
@@ -11,15 +11,46 @@ void Key_Init(void)
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
+uint8_t Key_Num;
+
 uint8_t Key_GetNum(void)
 {
-	uint8_t KeyNum = 0;
-	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0)
+	uint8_t Temp;
+	if (Key_Num)
 	{
-		Delay_ms(20);
-		while (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0);
-		Delay_ms(20);
-		KeyNum = 1;
+		Temp = Key_Num;
+		Key_Num = 0;
+		return Temp;
 	}
-	return KeyNum;
+	return 0;
+}
+
+uint8_t Key_GetState(void)
+{
+	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 1)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+void Key_Tick(void)
+{
+	static uint8_t Count;
+	static uint8_t CurrState, PrevState;
+	
+	Count ++;
+	if (Count >= 20)
+	{
+		Count = 0;
+		
+		PrevState = CurrState;
+		CurrState = Key_GetState();
+		
+		if (CurrState == 0 && PrevState != 0)
+		{
+			Key_Num = PrevState;
+		}
+	}
 }
